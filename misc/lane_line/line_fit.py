ff5d8a669bac2eb983bf0b21cd1eb168ebe7cc76
yeliang <yeliang@momenta.ai>
2022-06-06 17:20:24 +0800

    add ransac fitting

---
+++ misc/lane_line/line_fit.py
@@ -0,0 +1,80 @@
+import cv2
+import numpy as np
+from collections import defaultdict
+
+"""
+感知后处理曲线拟合优化：
+Input: 点集txt,每一行为 帧数_线id x,y x,y ...
+Output: coeff (c0, c1, c2, c3)
+"""
+
+
+# 点采样
+def ptSample(points):
+    pass
+
+
+# 曲线拟合
+def polyfit(points, deg=3):
+    x_arr = np.array([pt[1] for pt in points])
+    y_arr = np.array([pt[0] for pt in points])
+    coeff = np.polyfit(x_arr, y_arr, 3)
+    return coeff
+
+
+def vis(points, coeff):
+    """
+    Visualize the points and fitted curve
+    Args:
+        points(np.array):  set of (x,y) x is longitudinal, y is lateral
+        coeff(list|np.array): coeff of curve
+    """
+    showline_w = 600
+    showline_h = 640
+    show_w_scl = 30
+    show_h_scl = 5
+    showline = np.zeros((showline_w, showline_h), dtype=np.uint8)
+    cv2.putText(showline, "-det", (15, 24), cv2.FONT_HERSHEY_COMPLEX, 0.4, color=(0, 255, 0))
+
+    # initialize distance grid
+    dis_arr = np.arange(10, 110, 10)
+    for dis in dis_arr:
+        y = showline_h - dis * show_h_scl
+        x_arr = np.arange(50, showline_w, 50)
+        cv2.putText(showline, f"{dis}", (15, y), cv2.FONT_HERSHEY_COMPLEX, 0.4, color=(192, 192, 192))
+        for x in x_arr:
+            cv2.line(showline, (x, y), (x + 30, y), color=(192, 192, 192), thickness=1)
+    # cv2.imshow("debug", showline)
+    # cv2.waitKey()
+    # plot fitted line and raw points
+    for i, p in enumerate(points):  # x,y
+        x = coeff[0] + p[0] * coeff[1] + p[0] ** 2 * coeff[2] + p[0] ** 3 * coeff[3]  # y belong to lateral
+        x = -int(x * show_w_scl) + showline_w / 2
+        y = int(showline_h - p[0] * show_h_scl)
+        if i == 0:
+            p_s = (x, y)
+        else:
+            p_e = (x, y)
+            cv2.line(showline, p_s, p_e, color=(0, 255, 0), thickness=1)
+            p_s = p_e
+
+        center = (-int(show_w_scl * p[1] + showline_w / 2), y)
+        cv2.circle(showline, center, 2, (255, 255, 255), -1)
+
+
+if __name__ == '__main__':
+    vis(0, 0)
+
+    frames = defaultdict(defaultdict)
+    file = ""
+    with open(file, 'r') as f:
+        lines = f.readlines()
+
+    # 横轴，纵轴
+    for line in lines:
+        line.rstrip('\n').strip()
+        arr = line.split(" ")
+        frame, line_id = arr[0].split("_")
+        points = arr[1:]
+        points = [[float(p) for p in pt.split(',')] for pt in points]
+        frames[frame][line_id] = points
---
+++ misc/lane_line/ransac_fit.py
@@ -0,0 +1,110 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
+
+
+def linear_ransac_curve_fit(x, y):
+    x1 = np.array(x).reshape((-1, 1))
+    y1 = np.array(y).reshape((-1, 1))
+    xi = np.linspace(min(x), max(x), 500).reshape((-1, 1))
+
+    reg = linear_model.RANSACRegressor(linear_model.LinearRegression())
+    reg.fit(x1, y1)
+    yi = reg.predict(xi)
+    coeff = reg.estimator_.coef_
+    intercept = reg.estimator_.intercept_[0]
+    coeff = np.array([intercept, coeff[0, 0]])
+
+    inliers = reg.inlier_mask_
+    outliers = np.logical_not(inliers)
+
+    plt.plot(x[inliers], y[inliers], 'k.', label='inliers')
+    plt.plot(x[outliers], y[outliers], 'r.', label='outliers')
+    plt.plot(xi, yi, label='Linear Regression')
+
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Linear')
+    print('Equation: {0:.5f} + {1:.5f}x'.format(coeff[0], coeff[1]))
+    print('Y-intercept: {}'.format(coeff[0]))
+    plt.legend()
+    plt.show()
+
+
+def quadratic_ransac_curve_fit(x, y, name):
+    x1 = x.reshape((-1, 1))
+    y1 = y.reshape((-1, 1))
+
+    xi = np.linspace(min(x), max(x), 500).reshape((-1, 1))
+
+    poly_2 = PolynomialFeatures(degree=2)
+    x_2 = poly_2.fit_transform(x1)
+    xi_2 = poly_2.fit_transform(xi)
+
+    reg = linear_model.RANSACRegressor(linear_model.LinearRegression())
+    reg.fit(x_2, y1)
+    yi = reg.predict(xi_2)
+    coeff = reg.estimator_.coef_
+    intercept = reg.estimator_.intercept_[0]
+    coeff = np.array([intercept, coeff[0, 1], coeff[0, 2]])
+
+    inliers = reg.inlier_mask_
+    outliers = np.logical_not(inliers)
+
+    plt.plot(x[inliers], y[inliers], 'k.', label='inliers')
+    plt.plot(x[outliers], y[outliers], 'r.', label='outliers')
+    plt.plot(xi, yi, label='Quadratic Curve')
+
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Quadratic')
+    print('Equation: {0:.5f} + {1:.5f}x + {2:.5f}x^2'.format(coeff[0], coeff[1], coeff[2]))
+    print('Y-intercept: {}'.format(coeff[0]))
+    plt.legend()
+    plt.show()
+
+
+def cubic_ransac_curve_fit(x, y):
+    x1 = x.reshape((-1, 1))
+    y1 = y.reshape((-1, 1))
+
+    xi = np.linspace(min(x), max(x), 500).reshape((-1, 1))
+
+    poly_3 = PolynomialFeatures(degree=3)
+    x_3 = poly_3.fit_transform(x1)
+    xi_3 = poly_3.fit_transform(xi)
+
+    reg = linear_model.RANSACRegressor(linear_model.LinearRegression())
+    reg.fit(x_3, y1)
+    yi = reg.predict(xi_3)
+    coeff = reg.estimator_.coef_
+    intercept = reg.estimator_.intercept_[0]
+    coeff = np.array([intercept, coeff[0, 1], coeff[0, 2], coeff[0, 3]])
+
+    inliers = reg.inlier_mask_
+    outliers = np.logical_not(inliers)
+
+    plt.plot(x[inliers], y[inliers], 'k.', label='inliers')
+    plt.plot(x[outliers], y[outliers], 'r.', label='outliers')
+    plt.plot(xi, yi, label='Cubic Curve')
+
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Cubic')
+    print('Equation: {0:.5f} + {1:.5f}x + {2:.5f}x^2 + {3:.5f}x^3'.format(coeff[0], coeff[1], coeff[2], coeff[3]))
+    print('Y-intercept: {}'.format(coeff[0]))
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    # test data
+    n = 1000
+    xys = np.random.random((n, 2)) * 10
+    xys[:50, 1:] = xys[:50, :1]
+    x = xys[:, 0]
+    y = xys[:, 1]
+    linear_ransac_curve_fit(x, y)
+    quadratic_ransac_curve_fit(x, y, name=0)
+    cubic_ransac_curve_fit(xys[:, 0], xys[:, 1])
