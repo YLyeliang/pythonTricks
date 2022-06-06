7c2c20c97e8013635fcf6e22d93149b0142eede0
yeliang <yeliang@momenta.ai>
2022-06-06 19:51:50 +0800

    update video writer

--- opencv/video_write.py
+++ opencv/video_write.py
@@ -4,7 +4,7 @@ import cv2
 import os
 import os.path as osp
 import argparse
-
+from pathlib import Path
 from tqdm import tqdm
 
 
@@ -19,17 +19,22 @@ def parse_args():
 if __name__ == '__main__':
 
     args = parse_args()
-    img_dir = args.i
-    out_path = args.o
+    img_dir = args.input_dir
+    out_path = args.output_dir
     fps = args.fps
     img_list = os.listdir(img_dir)
+    img_list = [name for name in img_list if "track_point_un" in name]
     shape = cv2.imread(osp.join(img_dir, img_list[0])).shape[:2]  # H W
+
+    img_list = sorted(img_list, key=lambda x: int(x.split("_")[1]))
 
     # initialize video settings
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    if not osp.exists(out_path):
-        os.makedirs(out_path, exist_ok=True)
-    out = cv2.VideoWriter(out_path, fourcc, fps, shape[::-1])
+    out_path = Path(out_path)
+    pardir = out_path.parent.absolute()
+    if not osp.exists(pardir):
+        os.makedirs(pardir, exist_ok=True)
+    out = cv2.VideoWriter(str(out_path), fourcc, fps, shape[::-1])
 
     for img_p in tqdm(img_list):
         img = cv2.imread(os.path.join(img_dir, img_p))
